27b7e253516f866a64ba38fd03c6bf8567578f60
hodver <hodosi.veronika2113@gmail.com>
2024-03-21 12:31:17 +0100

    alpha snake

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            kígyóElem1 = new KígyóElem();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)kígyóElem1).BeginInit();
+            SuspendLayout();
+            // 
+            // kígyóElem1
+            // 
+            kígyóElem1.BackColor = Color.Fuchsia;
+            kígyóElem1.Location = new Point(121, 92);
+            kígyóElem1.Name = "kígyóElem1";
+            kígyóElem1.Size = new Size(20, 20);
+            kígyóElem1.TabIndex = 0;
+            kígyóElem1.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 500;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(kígyóElem1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)kígyóElem1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private KígyóElem kígyóElem1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Snake/Form1.cs
@@ -0,0 +1,83 @@
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        int fej_x = 100;
+        int fej_y = 100;
+        int irány_x = 1;
+        int irány_y = 0;
+        int lépésszám = 0;
+        int hossz = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lépésszám++;
+
+            //Fejnövesztés
+            fej_x += irány_x * KígyóElem.Méret;
+            fej_y += irány_y * KígyóElem.Méret;
+
+            foreach (KígyóElem item in Controls)
+            {
+                //Ha vannak már valami ott, ahova az új fejet tenném, vége a játéknak
+                if (item.Top == fej_y && item.Left == fej_x)
+                {
+                    timer1.Enabled = false;
+                    return;
+                }
+            }
+
+            KígyóElem ke = new KígyóElem();
+            ke.Top = fej_y;
+            ke.Left = fej_x;
+            Controls.Add(ke);
+
+            //Farokvágás
+            if (Controls.Count > hossz)
+            {
+                //KígyóElem levágandó = [0];
+                Controls.RemoveAt(0);
+            }
+
+            //if (lépésszám % 5 == 0) ++hossz;
+
+            //if (lépésszám % 2 == 0) ke.BackColor = Color.Yellow;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                irány_y = -1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                irány_y = 1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                irány_y = 0;
+                irány_x = -1;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                irány_y = 0;
+                irány_x = 1;
+            }
+        }
+    }
+}
