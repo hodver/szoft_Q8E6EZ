9440022e2c801364e0cfef46697cd6fe5e29a8de
hodver <hodosi.veronika2113@gmail.com>
2024-04-25 12:22:11 +0200

    connect

---
+++ RemotelDbTest/Form1.cs
@@ -0,0 +1,29 @@
+namespace RemotelDbTest
+{
+    public partial class Form1 : Form
+    {
+        Models.StudentContext studentContext = new Models.StudentContext();
+        public Form1()
+        {
+            InitializeComponent();
+            studentBindingSource.DataSource = studentContext.Students.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                studentContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.Message);
+            }
+        }
+    }
+}
